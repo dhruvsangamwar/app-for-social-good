a40c402dbddedd39cb50e4339d7adb2e7c86f5c5
Aroop Biswal <aroopbiswal@gmail.com>
2022-04-16 11:07:07 -0700

    changes

--- BudgetBetter/App.js
+++ BudgetBetter/App.js
@@ -4,7 +4,7 @@ import { StyleSheet, Text, View } from 'react-native';
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up mees.js to start working on your app!</Text>
+      <Text>Open up lololoollooaksdkajnsdl.js to start working on your app!</Text>
       <StatusBar style="auto" />
     </View>
   );
---
+++ screens/HomeScreen.js
@@ -0,0 +1,8 @@
+import React from 'react';
+import { Drawer, Router, Scene } from 'react-native-router-flux';
+
+const AppRouter = () => (
+    <Router>
+
+    </Router>
+);
