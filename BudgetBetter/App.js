5a28ab3eebd5feee7fcc2effc100a8ed3a3631be
Aroop Biswal <aroopbiswal@gmail.com>
2022-04-16 13:35:56 -0700

    changes

--- BudgetBetter/App.js
+++ BudgetBetter/App.js
@@ -3,26 +3,25 @@ import { StyleSheet, Text, View } from 'react-native';
 import { NavigationContainer } from '@react-navigation/native';
 import { createNativeStackNavigator } from '@react-navigation/native-stack';
 
-import HomeScreen from './screens/HomeScreen';
-import Splash from './screens/Splash';
+import { HomeScreen } from './screens/HomeScreen';
+import { Splash } from './screens/Splash';
 
 const Stack = createNativeStackNavigator();
 
 
-const App = () => {
+function App() {
   return (
-    <div>
     <NavigationContainer>
     <Stack.Navigator>
-    <Stack.Screen name="SplashScreen" component={Splash} />
-    <Stack.Screen name="HomeScreen" component={HomeScreen} />
+    {/* <Stack.Screen name="SplashScreen" component={Splash} /> */}
+    <Stack.Screen name="SplashScreen" component= { Splash }/>
+    <Stack.Screen name="HomeScreen" component= { HomeScreen }/>
     {/* <View style={styles.container}>
       <Text>Open up lololoollooaksdkajnsdl.js to start working on your app!</Text>
       <StatusBar style="auto" />
     </View> */}
     </Stack.Navigator>
     </NavigationContainer>
-    </div>
   );
 }
 
